5acdebeee57b8df9fecb0b17ad44d8de5a2b68ea
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-04-25 12:29:04 -0300

    Add homepage tests

--- pet-id/cypress/integration/home.spec.js
+++ pet-id/cypress/integration/home.spec.js
@@ -4,4 +4,20 @@ context('home tests', () => {
     it('certifies home page is loaded', () => {
         cy.visit('/')
     });
+    it('logs in', () => {
+    
+        cy.get('body').contains('Entrar').click();
+        cy.url().should('include', '/home');
+    });
+    it('signs up', () => {
+        cy.visit('/');
+        cy.get('body').contains('Cadastrar').click();
+        cy.url().should('include', 'sign-up');
+    });
+    it('go to sign up and go back to login screen', () => {
+        cy.visit('/');
+        cy.get('body').contains('Cadastrar').click();
+        cy.get('body').contains('Voltar').click();
+        cy.get('body').contains('Entrar');
+    });
 });
